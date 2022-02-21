f544fd51b60e1fd63832198e45cb1d22380a8d28
snowiewdev <snowiewongdev@gmail.com>
2022-02-22 00:56:49 +0800

    eye blinking

--- components/Eye.vue
+++ components/Eye.vue
@@ -9,6 +9,21 @@
 <script>
 export default {
   name: "Eye",
+  mounted() {
+    // this.initEyeBallMovement();
+  },
+  methods: {
+    // initEyeBallMovement() {
+    //   document.querySelector("body").addEventListener("mousemove", (event) => {
+    //     let eye = document.querySelector(".eye-inner");
+    //     let x = eye.getBoundingClientRect().left + eye.clientWidth / 4;
+    //     let y = eye.getBoundingClientRect().top + eye.clientHeight / 4;
+    //     let radian = Math.atan2(event.pageX - x, event.pageY - y);
+    //     let degree = radian * (180 / Math.PI) * -1 + 270;
+    //     eye.style.transform = `rotate(${degree}deg)`;
+    //   });
+    // },
+  },
 };
 </script>
 
@@ -19,11 +34,12 @@ export default {
   height: 40px;
   border: 2px solid $black;
   border-radius: 75% 15%;
-  transform: rotate(45deg);
+  // transform: rotate(45deg);
   overflow: hidden;
   display: flex;
   justify-content: center;
   align-items: center;
+  animation: blink 5s infinite alternate;
 
   .dark {
     border-color: $white;
@@ -58,6 +74,21 @@ export default {
   .iris {
     border-color: $white;
     background-color: $white;
+  }
+}
+
+@keyframes blink {
+  0% {
+    transform: scaleY(1) rotate(45deg);
+  }
+  3% {
+    transform: scaleY(0.1) rotate(45deg);
+  }
+  6% {
+    transform: scaleY(1) rotate(45deg);
+  }
+  100% {
+    transform: scaleY(1) rotate(45deg);
   }
 }
 </style>
